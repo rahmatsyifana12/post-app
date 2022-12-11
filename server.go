1167b92e7a84babb7c253d0301912f439dfa7bb6
rahmatsyifana12 <adean435@gmail.com>
2022-12-11 18:55:28 +0700

    Add db port and server port to env

--- server.go
+++ server.go
@@ -21,9 +21,10 @@ func main() {
 	db_user := os.Getenv("DB_USER")
 	db_password := os.Getenv("DB_PASSWORD")
 	db_name := os.Getenv("DB_NAME")
+	db_port := os.Getenv("DB_PORT")
 
 	// connect to database
-	dsn := db_user + ":" + db_password + "@tcp(127.0.0.1:3306)/" + db_name + "?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := db_user + ":" + db_password + "@tcp(127.0.0.1:" + db_port + ")/" + db_name + "?charset=utf8mb4&parseTime=True&loc=Local"
   	db, err := gorm.Open(mysql.New(mysql.Config{
 		DSN: dsn,
 	}), &gorm.Config{})
@@ -34,12 +35,9 @@ func main() {
 	// migrate all schema
 	db.AutoMigrate(&models.User{})
 
-	// create
-	db.Create(&models.User{ Username: "damian", Password: "damian", Name: "Damian", Phone: "08123456789"})
-
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
 }
